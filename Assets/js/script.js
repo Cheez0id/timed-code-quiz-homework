b66d1ade2d88da1f2c3c73683b601dd489fecc82
cheez0idgit <93955021+Chee0id@users.noreply.github.com>
2021-12-21 18:31:23 -0500

    adding questions for realism

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,34 +1,3 @@
-// //creating a coupe variables in js to get input from the form in html
-//  var freeResponse = document.querySelector("#randomResponse");
-// var nameInput = document.querySelector("#nameResponse");
-// var submitButton = document.querySelector("#submit");
-
-
-// // TODO: make function to log the text input from user upon button click
-
-// function logEntries(freeResponse, nameInput) {
-//     // Prevent default action
-//     // event.preventDefault();
-//     console.log(freeResponse + nameInput);
-//     // var response = "Thank you for your submission " + nameInput.value + "! You wrote this weird stuff in the first field: " + freeResponse.value + ".";
-//       // submissionResponseEl.textContent = response;
-// logEntries (freeResponse, nameInput) 
-//   }
-
-// //   console.log (freeResponse + nameInput + " <-- these were put into the form");
-// // var showResponse = console.log (freeResponse + nameInput);
-
-// // // Add listener to submit element
-// // submitButton.addEventListener("click", showResponse);
-
-
-
-
-
-
-
-
-
 
 //BEGINNING OF THE CODE
 
@@ -60,56 +29,61 @@ quizbox.appendChild(question1);
 // if class
 })
 
-// // get reference to the section tag.
-// // make a new paragraph tag.
-// var paragraph = document.createElement("p");
-// // add some text inside paragraph tag.
-// paragraph.textContent = "This is another paragraph";
-// // add paragrapgh inside div tag.
-// div.appendChild(paragraph);
-
-
-
-
-var form1 = document.querySelector (".form1") 
-var form2 = document.querySelector (".form2")
+//adding question variables individually to call to IDs created in quiz.html; this should have been done with arrays/split or something more efficient.
 var question1 = document.querySelector ("#question1")
 var question2 = document.querySelector ("#question2")
 var question3 = document.querySelector ("#question3")
 var question4 = document.querySelector ("#question4")
 var question5 = document.querySelector ("#question5")
 
-//TODO: MAKE A LOOP THAT SHOWS EACH QUESTION IN THE BANK AS QUESTIONS
-
-
+//The below functions make it so that when a botton is clicked, the next question shows; if the answer is incorrect, time is removed. <and needs work>
 
 function hidequestion1 () {
   question1.style.display = "none";
   quizbox.appendChild(question2)
-  
-  // console.log (this.target.className)
   var cl=event.target.innerHTML;
-  if (cl != "it is an "){
+  if (cl != "it is an identifier"){
     timeleft -= 10
   }
   console.log (cl)
 }
 function hidequestion2 () {
-  question2.style.visibility = "hidden";
+  question2.style.display = "none";
+  quizbox.appendChild(question3)
+  var cl=event.target.innerHTML;
+  if (cl != "it is a class"){
+    timeleft -= 10
+  }
+  console.log (cl)
 }
 function hidequestion3 () {
-  question3.style.visibility = "hidden";
+  question3.style.display = "none";
+  quizbox.appendChild(question4)
+  var cl=event.target.innerHTML;
+  if (cl != "it is an identifier"){
+    timeleft -= 10
+  }
+  console.log (cl)
 }
 function hidequestion4 () {
-  question4.style.visibility = "hidden";
+  question4.style.display = "none";
+  quizbox.appendChild(question5)
+  var cl=event.target.innerHTML;
+  if (cl != "it is an identifier"){
+    timeleft -= 10
+  }
+  console.log (cl)
 }
 function hidequestion5 () {
-  question5.style.visibility = "hidden";
+  question5.style.display = "none";
+  quizbox.appendChild(END)
 }
 
 
 
 
+
+//END OF CODE, DISREGARD THE BELOW NOTES
 
 
 
@@ -128,3 +102,20 @@ function hidequestion5 () {
 //  form1.style.display = "none"
 //  console.log ("click?")
 // })}
+
+
+// function logEntries(freeResponse, nameInput) {
+//     // Prevent default action
+//     // event.preventDefault();
+//     console.log(freeResponse + nameInput);
+//     // var response = "Thank you for your submission " + nameInput.value + "! You wrote this weird stuff in the first field: " + freeResponse.value + ".";
+//       // submissionResponseEl.textContent = response;
+// logEntries (freeResponse, nameInput) 
+//   }
+
+// //   console.log (freeResponse + nameInput + " <-- these were put into the form");
+// // var showResponse = console.log (freeResponse + nameInput);
+
+// // // Add listener to submit element
+// // submitButton.addEventListener("click", showResponse);
+
